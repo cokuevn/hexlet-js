d33af77e4aa237566dce18ac47ececd44d049ac6
Islam Tsokuev <cokuevn@gmail.com>
2022-07-02 16:42:00 +0300

    add new file

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-console.log('Hello, Hexlet!');
-console.log('11111')
+import _ from "lodash";
+
+
+let arr = _.intersection([2, 1], [2, 3]);
+let str = _.capitalize("hello");
+console.log(_.last(['one', 'two']));
+console.log(arr)
+console.log(str)
