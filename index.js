39fc72a91249f9e074f27ea4c7c5597b94413a2c
Islam Tsokuev <cokuevn@gmail.com>
2022-07-02 23:28:10 +0300

    install prettier

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 import _ from "lodash";
-
-
-let arr = _.intersection([2, 1], [2, 3]);
-let str = _.capitalize("hello");
-console.log(_.last(['one', 'two']));
-console.log(arr)
-console.log(str)
+const fn = (a, b) => {
+  const c = a + b;
+  return c;
+};
